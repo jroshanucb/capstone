2af3f2977bed607604c81b744118da87c2b03e94
stevendleung <stevendleung@berkeley.edu>
2021-11-30 20:46:47 -0500

    model inference work

--- models/run_models.py
+++ models/run_models.py
@@ -277,7 +277,6 @@ def yolo_count_blank_detect_formatting(formatted_yolo, model_id):
     for image in range(1,4):
         formatted_yolo['image_id_{}_blank'.format(image)] = formatted_yolo['image_id_{}_species_name'.format(image)].apply\
                                                             (lambda x:True if x == '' else False)
-
     #Load date
     now = datetime.now() # current date and time
     date_string = now.strftime("%m/%d/%Y")
@@ -289,9 +288,9 @@ def yolo_count_blank_detect_formatting(formatted_yolo, model_id):
     return formatted_yolo
 
 def variance_of_laplacian(image):
-      # compute the Laplacian of the image and then return the focus
-      # measure, which is simply the variance of the Laplacian
-      return cv2.Laplacian(image, cv2.CV_64F).var()
+    # compute the Laplacian of the image and then return the focus
+    # measure, which is simply the variance of the Laplacian
+    return cv2.Laplacian(image, cv2.CV_64F).var()
 
 d = {'img_id':[], 'blurry':[], 'blurry_index':[]}
 
@@ -326,7 +325,7 @@ def blur_processing(img_directory, formatted_yolo, run_blur):
         blur_df = pd.DataFrame(d)
 
         blur_df['image_group_id'] = blur_df['img_id'].apply(lambda x: img_name_to_event(x))
-        blur_df['img_appendix'] = blur_df['img_id'].apply(lambda x: x.split('.')[0][-1])  
+        blur_df['img_appendix'] = blur_df['img_id'].apply(lambda x: x.split('.')[0][-1])
 
         for i in range(1,4):
             formatted_yolo = pd.merge(formatted_yolo, blur_df[['image_group_id', 'img_appendix', 'blurry']],
@@ -342,3 +341,163 @@ def blur_processing(img_directory, formatted_yolo, run_blur):
             formatted_yolo['image_id_{}_detectable'.format(i)] = False
 
     return formatted_yolo
+
+
+#EfficientNet
+def get_speciesname_from_id(id, phase):
+
+    if phase == 'phase1':
+        speciesList = ['animal', 'blank']
+    else:
+        speciesList =  ['bear', 'blank', 'cottontail_snowshoehare', 'coyote', 'deer', 'elk', 'foxgray_foxred', 'opossum', 'raccoon', 'turkey', 'wolf']
+    idx = int(id)
+    if idx > 10 or idx < 0:
+        speciesName = 'other'
+    else:
+        speciesName = speciesList[idx]
+    return speciesName
+
+def load_effnet_json(output_json, phase = 'phase2'):
+    effnet_json = {}
+
+    with open(output_json) as json_file:
+        data = json.load(json_file)
+
+    data = data['{}_classification_results'.format(phase)]
+    for dict_list in data:
+        value = dict_list
+        newKey = value['id']
+        effnet_json[newKey] = {}
+        class_list = ""
+        conf_list = ""
+
+        for key2, value2 in value['conf_dict'].items():
+            class_list += get_speciesname_from_id(key2, phase) + ","
+            conf_list += str(value2[0]) + ","
+
+        effnet_json[newKey]['Class'] = class_list[:-1]
+        effnet_json[newKey]['Conf'] = conf_list[:-1]
+
+        # print(effnet_json)
+    return effnet_json
+
+
+def format_effnet(effnet_dict, model_id):
+    '''Convert effnet dict to pandas df'''
+    event_list = []
+    image_list = []
+    class_list = []
+    conf_list = []
+
+    for key, value in effnet_dict.items():
+        event_list.append(img_name_to_event(key))
+        image_list.append(key)
+        class_list.append(value['Class'])
+        conf_list.append(value['Conf'])
+
+    effnet_int_df = pd.DataFrame({'event':event_list,
+                 'image':image_list,
+                 'class': class_list,
+                 'conf': conf_list}).sort_values(by='image')
+
+    image_group_id = []
+    image_id_1 = []
+    image_id_2 = []
+    image_id_3 = []
+    image_id_1_species_name = []
+    image_id_2_species_name = []
+    image_id_3_species_name = []
+    image_id_1_conf = []
+    image_id_2_conf = []
+    image_id_3_conf = []
+    image_id_1_bbox = []
+    image_id_2_bbox = []
+    image_id_3_bbox = []
+
+    for event in effnet_int_df['event'].unique():
+        image_group_id.append(event)
+        event_effnet = effnet_int_df[effnet_int_df['event'] == event]
+
+        i = 1
+        for row, values in event_effnet.iterrows():
+            image_appendix = values['image'].split('.')[0][-1]
+            if model_id == 2:
+                class_for_row = values['class'].split(',')[0]
+                conf_for_row = values['conf'].split(',')[0]
+            else:
+                class_for_row = values['class']
+                conf_for_row = values['conf']
+
+            if i ==1:
+                image_id_1.append(image_appendix)
+                image_id_1_species_name.append(class_for_row)
+                image_id_1_conf.append(conf_for_row)
+            elif i == 2:
+                image_id_2.append(image_appendix)
+                image_id_2_species_name.append(class_for_row)
+                image_id_2_conf.append(conf_for_row)
+            elif i == 3:
+                image_id_3.append(image_appendix)
+                image_id_3_species_name.append(class_for_row)
+                image_id_3_conf.append(conf_for_row)
+            i+=1
+
+        if i == 3:
+            image_id_3.append('')
+            image_id_3_species_name.append('')
+            image_id_3_conf.append('')
+        if i == 2:
+            image_id_2.append('')
+            image_id_2_species_name.append('')
+            image_id_2_conf.append('')
+
+            image_id_3.append('')
+            image_id_3_species_name.append('')
+            image_id_3_conf.append('')
+
+    formatted_effnet = pd.DataFrame({'image_group_id':image_group_id,
+        'image_id_1':image_id_1,
+        'image_id_2':image_id_2,
+        'image_id_3':image_id_3,
+        'image_id_1_species_name': image_id_1_species_name,
+        'image_id_2_species_name':image_id_2_species_name,
+        'image_id_3_species_name' :image_id_3_species_name,
+        'image_id_1_conf': image_id_1_conf,
+        'image_id_2_conf': image_id_2_conf,
+        'image_id_3_conf': image_id_3_conf})
+
+    ['image_id_1_blank']
+
+    for image in range(1,4):
+        formatted_effnet['image_id_{}_count'.format(image)] = 0
+
+    for image in range(1,4):
+        formatted_effnet['image_id_{}_bbox'.format(image)] = 0
+
+
+
+    for image in range(1,4):
+        if model_id == 2:
+            formatted_effnet['image_id_{}_blank'.format(image)] = formatted_effnet['image_id_{}_species_name'.format(image)].apply\
+                                                            (lambda x:True if x == 'blank' else False)
+        else:
+            formatted_effnet['image_id_{}_blank'.format(image)] = formatted_effnet['image_id_{}_species_name'.format(image)].apply\
+                                                            (lambda x:True if x == '' else False)
+
+    for image in range(1,4):
+        formatted_effnet['image_id_{}_detectable'.format(image)] = False
+
+    formatted_effnet['model_id'] = model_id
+
+    return formatted_effnet
+
+stage2_output_json = '../results/JSON_txt_outputs/phase2_efficientnetb5_yolo_splits4-1_classifications_basePlusblanks.json'
+
+stage2_effnet_dict = load_effnet_json(stage2_output_json, 'phase2')
+stage2_formatted_effnet = format_effnet(stage2_effnet_dict, 4)
+
+
+stage1_output_json = '../results/JSON_txt_outputs/phase1_efficientnetb0_classifications_yolosplits_4-1.json'
+
+stage1_effnet_dict = load_effnet_json(stage1_output_json, 'phase1')
+stage1_formatted_effnet = format_effnet(stage1_effnet_dict, 2)
