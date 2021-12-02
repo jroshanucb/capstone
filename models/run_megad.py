b35e5d2ad69a311ee37c5cfef12dcce2fbdfbbe8
stevendleung <stevendleung@berkeley.edu>
2021-12-01 20:00:08 -0500

    update megad

--- models/run_megad.py
+++ models/run_megad.py
@@ -14,7 +14,7 @@ os.system('echo "PYTHONPATH: $PYTHONPATH"')
 def run_md_inference(images_dir, output_file_path):
 
 
-    os.system('python run_tf_detector_batch.py megadetector_v4_1_0.pb "{}"\
+    os.system('python run_tf_detector_batch.py md_v4_1_0.pb "{}"\
      "{}" --recursive'.format(images_dir, output_file_path))
 
 def reformat_md_json(images_dir, output_file_path):
