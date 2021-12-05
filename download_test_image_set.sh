22202d0c201833f6e633be7753cd1de0d4f07569
stevendleung <stevendleung@berkeley.edu>
2021-12-05 13:20:42 -0500

    Update download_test_image_set.sh

--- download_test_image_set.sh
+++ download_test_image_set.sh
@@ -1,3 +1,4 @@
 #Download full test images zip file from gdrive
 wget --no-check-certificate --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies  'https://docs.google.com/uc?export=download&id=1RRfK4wMBpvjAiyP_gjL2xOAfhOmXOcYO' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1RRfK4wMBpvjAiyP_gjL2xOAfhOmXOcYO" -O yolo_splits4.2.zip && rm -rf /tmp/cookies.txt
-unzip yolo_splits4.2.zip
+unzip yolo_splits4.2.zip ../
+rm yolo_splits4.2.zip
